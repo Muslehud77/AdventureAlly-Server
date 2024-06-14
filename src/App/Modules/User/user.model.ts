eadcdc7df9a0131620365e28a6ebaaa35e4e0a9f
Muslehud77 <fardinmohit@gmail.com>
2024-06-14 12:41:13 +0800

    added the required env values and imported into configs file, included hashpassword while saving the user

--- src/App/Modules/User/user.model.ts
+++ src/App/Modules/User/user.model.ts
@@ -1,6 +1,9 @@
 import { Schema, model } from 'mongoose';
 import { TUser, TUserStatics } from './user.interface';
 import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import bcrypt from 'bcrypt';
+import configs from '../../configs';
 
 const userSchema = new Schema<TUser, TUserStatics>(
   {
@@ -20,10 +23,19 @@ const userSchema = new Schema<TUser, TUserStatics>(
 userSchema.statics.isUserExists = async function(id:string){
     const user = await User.findById({_id:id})
     if(!user){
-        throw new AppError()
+        throw new AppError(httpStatus.NOT_FOUND,"User does not exist!")
     }
     return user
 }
+
+
+userSchema.pre('save',async function(){
+    const password = this?.password
+
+    const encryptedPassword = await bcrypt.hash(password,configs.saltRounds)
+
+    this.password = encryptedPassword;
+})
 
 
 export const User = model<TUser, TUserStatics>('User', userSchema);
--- src/App/configs/index.ts
+++ src/App/configs/index.ts
@@ -4,7 +4,22 @@ dotenv.config()
 
 const port = process.env.PORT
 const dbUri  = process.env.DB_URI as string
+const saltRounds = Number(process.env.HASH_SALT) as number;
+const defaultPassword = process.env.DEFAULT_PASS as string;
+const node_env = process.env.NODE_ENV as string;
+const jwt_access_secret = process.env.JWT_ACCESS_SECRET as string;
+const jwt_refresh_secret = process.env.JWT_REFRESH_SECRET as string;
+const jwt_access_expiresIn = process.env.JWT_ACCESS_EXPIRES_IN as string;
+const jwt_refresh_expiresIn = process.env.JWT_REFRESH_EXPIRES_IN as string; 
 
-
-
-export default { port, dbUri };
+export default {
+  dbUri,
+  port,
+  saltRounds,
+  defaultPassword,
+  node_env,
+  jwt_access_secret,
+  jwt_refresh_secret,
+  jwt_access_expiresIn,
+  jwt_refresh_expiresIn,
+};
